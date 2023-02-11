92f582537475d5a4219c4b801f564ef23d45fc2b
subject026 <subject026@protonmail.com>
2023-02-11 10:58:59 +0000

    add and configure website ui library

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: ["@breadchain.xyz/website-ui"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- apps/website/src/pages/about.tsx
+++ apps/website/src/pages/about.tsx
@@ -1,19 +1,18 @@
+import { Header } from "@breadchain.xyz/website-ui";
 import Head from "next/head";
 
-import { Header } from "@breadchain.xyz/website-ui";
-
-export default function Home() {
+export default function About() {
   return (
     <>
       <Head>
-        <title>Home Page</title>
-        <meta name="description" content="Home Page" />
+        <title>About Page</title>
+        <meta name="description" content="About Page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className="min-h-screen p-16 font-redhat">
-        <h1 className="font-pressstart text-2xl">Home</h1>
+        <h1 className="font-pressstart text-2xl">About</h1>
         <p>
           Get started by editing&nbsp;
           <code>src/pages/index.tsx</code>
