44f1bdc9e3295565dc68b085dfc1e2a8adee500b
subject026 <subject026@protonmail.com>
2023-04-02 13:22:30 +0100

    add values center bg graphic

---
+++ packages/site-ui/components/Graphics/index.tsx
@@ -0,0 +1,101 @@
+export function ValuesCenter() {
+  return (
+    <div className="w-32 h-32 hidden sm:block">
+      <svg
+        viewBox="0 0 215 215"
+        className="w-full h-full fill-none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="105.4"
+          y="111.4"
+          width="4.2"
+          height="99.2"
+          rx="0.6"
+          stroke="url(#paint0_linear_1468_1361)"
+          stroke-opacity="0.5"
+          stroke-width="0.8"
+        />
+        <rect
+          x="4.4"
+          y="109.731"
+          width="4.2"
+          height="99.2"
+          rx="0.6"
+          transform="rotate(-90 4.4 109.731)"
+          stroke="url(#paint1_linear_1468_1361)"
+          stroke-opacity="0.5"
+          stroke-width="0.8"
+        />
+        <rect
+          x="111.4"
+          y="109.731"
+          width="4.2"
+          height="99.2"
+          rx="0.6"
+          transform="rotate(-90 111.4 109.731)"
+          stroke="url(#paint2_linear_1468_1361)"
+          stroke-opacity="0.5"
+          stroke-width="0.8"
+        />
+        <rect
+          x="109.6"
+          y="103.6"
+          width="4.2"
+          height="99.2"
+          rx="0.6"
+          transform="rotate(-180 109.6 103.6)"
+          stroke="url(#paint3_linear_1468_1361)"
+          stroke-opacity="0.5"
+          stroke-width="0.8"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_1468_1361"
+            x1="107.5"
+            y1="111"
+            x2="107.5"
+            y2="211"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#ED7BC7" />
+            <stop offset="1" stop-color="#ED7BC7" stop-opacity="0" />
+          </linearGradient>
+          <linearGradient
+            id="paint1_linear_1468_1361"
+            x1="6.5"
+            y1="110.131"
+            x2="6.5"
+            y2="210.131"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#ED7BC7" stop-opacity="0" />
+            <stop offset="1" stop-color="#ED7BC7" />
+          </linearGradient>
+          <linearGradient
+            id="paint2_linear_1468_1361"
+            x1="113.5"
+            y1="110.131"
+            x2="113.5"
+            y2="210.131"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#ED7BC7" />
+            <stop offset="1" stop-color="#ED7BC7" stop-opacity="0" />
+          </linearGradient>
+          <linearGradient
+            id="paint3_linear_1468_1361"
+            x1="112.5"
+            y1="104"
+            x2="112.5"
+            y2="204"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#ED7BC7" />
+            <stop offset="1" stop-color="#ED7BC7" stop-opacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  );
+}
--- packages/site-ui/components/Values/Values.tsx
+++ packages/site-ui/components/Values/Values.tsx
@@ -5,6 +5,7 @@ import { pageWrap, pageWrapPadding } from "../classStrings";
 import { SectionTitle } from "../SectionTitle";
 import { SectionSubtitle } from "../SectionSubtitle";
 import { ValueCard, ValueIcons } from "./ValueCard";
+import { ValuesCenter } from "../Graphics";
 
 interface IValue {
   title: string;
@@ -18,7 +19,7 @@ export function Values({ values }: { values: IValue[] }) {
       id="values"
       className={classNames(pageWrap, pageWrapPadding, "py-16")}
     >
-      <div className="flex flex-col sm:items-center gap-4">
+      <div className="flex flex-col sm:items-center gap-4 pb-16">
         <div className="flex flex-col items-start sm:flex-row gap-2">
           <SectionTitle>
             <span className="font-normal inline-block pr-2">Currency</span>
@@ -30,7 +31,10 @@ export function Values({ values }: { values: IValue[] }) {
           missions.
         </SectionSubtitle>
       </div>
-      <div className="max-w-4xl m-auto grid grid-cols-2 pt-16 gap-4 sm:gap-16 md:gap-x-48 text-center">
+      <div className="max-w-4xl m-auto grid grid-cols-2 gap-4 sm:gap-16 md:gap-x-48 text-center relative">
+        <div className="absolute left-0 right-0 top-0 bottom-0 flex items-center justify-center">
+          <ValuesCenter />
+        </div>
         {values.map(({ title, details, icon }, i) => {
           return (
             <ValueCard
