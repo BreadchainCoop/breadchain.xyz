f1d27da1b5cec90c5f6ae0380f558ff3d1aaa5d9
subject026 <subject026@protonmail.com>
2023-04-30 17:22:17 +0100

    fix hero image shadow

--- packages/site-ui/components/Hero/MidjourneyImage.tsx
+++ packages/site-ui/components/Hero/MidjourneyImage.tsx
@@ -2,10 +2,17 @@ import React from "react";
 
 export function MidjourneyImage() {
   return (
-    <img
-      className="w-[24rem] md:w-[40rem] transform -translate-y-14 sm:-translate-y-8 opacity-60 min-[432px]:opacity-100 translate-x-48 min-[432px]:translate-x-42 min-[490px]:translate-x-32 sm:translate-x-24 md:translate-x-64 lg:translate-x-32"
-      src="/img/hero-bread.png"
-      alt=""
-    />
+    <>
+      <img
+        className="w-[24rem] md:w-[40rem] transform -translate-y-14 sm:-translate-y-8 opacity-60 min-[432px]:opacity-100 translate-x-48 min-[432px]:translate-x-42 min-[490px]:translate-x-32 sm:translate-x-24 md:translate-x-64 lg:translate-x-32 dark:hidden"
+        src="/img/hero_bread_light.png"
+        alt=""
+      />
+      <img
+        className="w-[24rem] md:w-[40rem] transform -translate-y-14 sm:-translate-y-8 opacity-60 min-[432px]:opacity-100 translate-x-48 min-[432px]:translate-x-42 min-[490px]:translate-x-32 sm:translate-x-24 md:translate-x-64 lg:translate-x-32 hidden dark:block"
+        src="/img/hero_bread_dark.png"
+        alt=""
+      />
+    </>
   );
 }
