a547b01fe9f2e453c365c2a4c150fbf45aba0a4d
subject026 <subject026@protonmail.com>
2024-05-20 11:38:52 +0100

    import tw theme

--- apps/app/tailwind.config.ts
+++ apps/app/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import breadchainTheme from "@breadchain.xyz/theme";
 
 const config: Config = {
   content: [
@@ -6,15 +7,11 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    ...breadchainTheme,
   },
   plugins: [],
 };
+
 export default config;
