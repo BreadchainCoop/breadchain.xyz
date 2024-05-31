461884dc64f13332f8d0dd36c1597c86363bd122
subject026 <subject026@protonmail.com>
2024-05-31 16:39:56 +0200

    add info cards

---
+++ apps/site-new/src/components/Icons/Coin.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function CoinIcon() {
+  return (
+    <svg
+      className="h-full w-full fill-current"
+      viewBox="0 0 65 65"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M14.6 2h37.8v6.3H14.6V2ZM8.3 14.6V8.3h6.3v6.3H8.3Zm0 37.8V14.6H2v37.8h6.3Zm6.3 6.3v-6.3H8.3v6.3h6.3Zm37.8 0V65H14.6v-6.3h37.8Zm6.3-6.3v6.3h-6.3v-6.3h6.3Zm0-37.8H65v37.8h-6.3V14.6Zm0 0V8.3h-6.3v6.3h6.3Zm-28.35-3.15h6.3v6.3h9.45v6.3H27.2v6.3h18.9v18.9h-9.45v6.3h-6.3v-6.3H20.9v-6.3h18.9v-6.3H20.9v-18.9h9.45v-6.3Z"
+      />
+    </svg>
+  );
+}
--- apps/site-new/tailwind.config.mjs
+++ apps/site-new/tailwind.config.mjs
@@ -6,6 +6,20 @@ export default {
   darkMode: "selector",
   theme: {
     extend: {
+      backgroundImage: {
+        "card-pattern-1": "url('/img/infocard-bg-1.svg')",
+        "card-pattern-2": "url('/img/infocard-bg-2.svg')",
+        "card-pattern-3": "url('/img/infocard-bg-3.svg')",
+      },
+      gridTemplateColumns: {
+        "infocards-sm": "2.5rem auto 2.5rem",
+        "infocards-md":
+          "2.5rem minmax(0, 1fr) 2.5rem minmax(0, 1fr) 2.5rem minmax(0, 1fr) 2.5rem",
+      },
+      gridTemplateRows: {
+        "infocards-sm": "auto auto auto",
+        "infocards-md": "2.5rem auto 2.5rem 14rem",
+      },
       ...theme,
     },
   },
